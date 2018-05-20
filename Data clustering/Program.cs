8ac83b389ae362956dfdf509fe8aff4eaf927c9d
Szymon Darmofał <darmofal.szymon@gmail.com>
2018-05-20 11:53:18 +0200

    Working alghoritm with decide on nearest points distance about which groups are nearest.

--- Data clustering/Program.cs
+++ Data clustering/Program.cs
@@ -18,6 +18,14 @@ namespace Data_clustering
             this.X = x;
             this.Y = y;
         }
+
+        public static double CalculateEuclideanMetric(Point point1, Point point2)
+        {
+            return Math.Sqrt(
+                (point2.X - point1.X) * (point2.X - point1.X) +
+                (point2.Y - point1.Y) * (point2.Y - point1.Y)
+            );
+        }
     }
 
     class Program
@@ -32,17 +40,79 @@ namespace Data_clustering
             return parsedValue;
         }
 
-        static List<Point> ReadFromFile(string file)
+        static List<List<Point>> ReadFromFile(string file)
         {
-            var list = new List<Point>();
+            var list = new List<List<Point>>();
 
             foreach (var line in File.ReadAllLines(file))
             {
                 var values = line.Split(' ');
-                list.Add(new Point(StringToDouble(values[0]), StringToDouble(values[1])));
+                Point point = new Point(StringToDouble(values[0]), StringToDouble(values[1]));
+                List<Point> pointList = new List<Point>();
+                pointList.Add(point);
+                list.Add(pointList);
             }
 
             return list;
+        }
+
+        static Dictionary<string, List<Point>> FindNearestGroups(List<List<Point>> groups)
+        {
+            SortedDictionary<double, Dictionary<string, List<Point>>> groupsDistance = new SortedDictionary<double, Dictionary<string, List<Point>>>();
+
+            foreach (List<Point> group in groups)
+            {
+                List<List<Point>> currentGroups = new List<List<Point>>(groups);
+                currentGroups.Remove(group);
+                var nearest = FindNearestGroup(currentGroups, group);
+                Dictionary<string, List<Point>> dictBuilder = new Dictionary<string, List<Point>>();
+                dictBuilder.Add("current", group);
+                dictBuilder.Add("another", nearest.Value);
+                if (!groupsDistance.Keys.Contains(nearest.Key))
+                {
+                    groupsDistance.Add(nearest.Key, dictBuilder);
+                }
+            }
+            return groupsDistance.First().Value;
+        }
+
+        static KeyValuePair<double, List<Point>> FindNearestGroup(List<List<Point>> groups, List<Point> currentGroup)
+        {
+            SortedDictionary<double, List<Point>> nearestPointsInGroups = new SortedDictionary<double, List<Point>>();
+            SortedDictionary<double, List<Point>> nearestGroups = new SortedDictionary<double, List<Point>>();
+
+            foreach (List<Point> group in groups)
+            {
+                nearestPointsInGroups.Clear();
+                foreach (Point currentGroupPoint in currentGroup)
+                {
+                    //TODO: Find average point
+                    // Nearest point
+                    foreach (Point anotherGroupPoint in group)
+                    {
+                        double metric = Point.CalculateEuclideanMetric(currentGroupPoint, anotherGroupPoint);
+                        if (! nearestPointsInGroups.Keys.Contains(metric))
+                        {
+                            nearestPointsInGroups.Add(metric, group);
+                        }
+                    }
+                }
+                nearestGroups.Add(nearestPointsInGroups.Keys.First(), nearestPointsInGroups.Values.First());
+            }
+
+            return nearestGroups.First();
+        }
+
+        static void PrintGroups(List<List<Point>> groups)
+        {
+            foreach (List<Point> group in groups)
+            {
+                Console.WriteLine("GROUP: ");
+                foreach (Point point in group)
+                {
+                    Console.WriteLine("\t" + point.X + "\t" + point.Y);
+                }
+            }
         }
 
         static void Main(string[] args)
@@ -50,7 +120,19 @@ namespace Data_clustering
             string DATA_ROOT_PATH = @"D:\Programowanie\C#\Sztuczna inteligencja\Data clustering\Data clustering\data\";
             string filename = @"spiral.txt";
 
-            List<Point> points = ReadFromFile(DATA_ROOT_PATH + filename);
+            List<List<Point>> groups = ReadFromFile(DATA_ROOT_PATH + filename);
+
+            do
+            {
+                var nearestGroups = FindNearestGroups(groups);
+                groups.Remove(nearestGroups["current"]);
+                groups.Remove(nearestGroups["another"]);
+                nearestGroups["current"].AddRange(nearestGroups["another"]);
+                groups.Add(nearestGroups["current"]);
+            } while (groups.Count > 3);
+
+            PrintGroups(groups);
+            Console.ReadKey();
         }
     }
 }
